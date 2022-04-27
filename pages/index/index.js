69bbb6151a8248e57adfb785f39b63d47f7d82ce
ChanKinHo <876204671@qq.com>
2022-04-27 17:32:04 +0800

    轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,14 +4,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    //轮播图数组
+    swiperList:[
+      {
+        image_src:"../../pics/banner1.png",
+        id:1
+      },
+      {
+        image_src:"../../pics/banner2.png",
+        id:2
+      },
+      {
+        image_src:"../../pics/banner3.png",
+        id:3
+      }
+    ]
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 生命周期函数--监听页面加载, 页面开始加载就会触发
    */
   onLoad: function (options) {
-    
+    //1、发送异步请求获取轮播图
+    // var reqTask = wx.request({
+    //   url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+    //   data: {},
+    //   header: {'content-type':'application/json'},
+    //   method: 'GET',
+    //   dataType: 'json',
+    //   responseType: 'text',
+    //   success: (result) => {
+    //     this.setData({
+    //       swiperList:result.data.message
+    //     })
+    //   },
+    //   fail: () => {},
+    //   complete: () => {}
+    // });
+      
   },
 
   /**
